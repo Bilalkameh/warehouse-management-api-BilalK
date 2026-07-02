485e11e4136c1c56b5ea18571afba6c22520e259
Bilal <bilal_kameh@hotmail.com>
2026-07-02 21:44:25 +0300

    refactor(session-02): improve model and DTO validation

--- warehouse-management/warehouse-management/Models/Product.cs
+++ warehouse-management/warehouse-management/Models/Product.cs
@@ -4,27 +4,31 @@ namespace warehouse_management.Models;
 
 public class Product
 {
-    [Required]
-    [StringLength(50,MinimumLength = 1)]
+    [Required(ErrorMessage = "Id required")]
+    [StringLength(50,MinimumLength = 1, ErrorMessage = "Id must be between 1 and 50 characters." )]
     public string Id { get; set; }
     
-    [Required]
-    [StringLength(40)]
+    [Required(ErrorMessage = "Product name required.")]
+    [StringLength(40, MinimumLength = 1,ErrorMessage = "Product name must be between 1 and 40 characters.")]
     public string Name { get; set; }
     
     
-    [StringLength(25)]
+    [StringLength(25, ErrorMessage = "SKU cannot exceed 25 characters.")]
     public string? SKU { get; set; }
     
-    
+    [Required(ErrorMessage = "Product must have a description.")]
+    [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
     public string Description { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = "Product must have a price.")]
+    [Range(0.01, double.MaxValue,ErrorMessage = "Price must be greater than zero.")]
     public double Price { get; set; }
     
+    [Required(ErrorMessage = "Quantity required.")]
+    [Range(0, int.MaxValue,ErrorMessage = "Quantity cannot be negative.")]
     public int QuantityInStock { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Supplier name required.")]
     public string SupplierName { get; set; }
     public DateTime ExpiryDate { get; set; }
     public bool IsArchived { get; set; }
